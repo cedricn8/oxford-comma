f24373b522f722b7586f47f092afdb4f923ce48d
cedricn8 <cedcn-9mail@yahoo.com>
2022-08-30 02:37:12 -0700

    commit tree f95948fd2b0661ffa41db2f67b8d73302f0d7114

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    number = array.size
+    if number > 2    
+        last_item = array.pop
+        new_string = array.join(", ")
+        new_string = new_string + ", and " + last_item
+    elsif number == 2
+        new_string = array.join(" and ")
+    else
+        new_string = array.join
+    end
 end
